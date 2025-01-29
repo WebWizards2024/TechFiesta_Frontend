bc24a677248e867c7a78047b35746efc58724bd7
Sarvesh Naik <naiksarvesh463@gmail.com>
2025-01-29 22:18:20 +0530

    React query setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-import './App.css'
-import Home from './components/Home/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AddProfile from './components/Profile/AddProfile';
-import Container from './components/Container/Container';
-import { AuthProvider } from './context/AuthProvider'
-import Login from './components/Login';
-import Register from './components/Register';
+import "./App.css";
+import Home from "./components/Home/Home";
+import AddProfile from "./components/Profile/AddProfile";
+import Container from "./components/Container/Container";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * 60 * 1000,
+    },
+  },
+});
 
 function App() {
-
-
   return (
-    <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
 
-        <Route path="/container" element={<Container />}>
-          <Route path="profile" element={<AddProfile />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
-    </AuthProvider>
-  )
+            <Route path="/container" element={<Container />}>
+              <Route path="profile" element={<AddProfile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
