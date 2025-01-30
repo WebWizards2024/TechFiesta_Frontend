aa9bc460fa5f5a94c73680849ca9f69bf914d15f
chaitanya girgaonkar <girgaonkar.chaitanya@gmail.com>
2025-01-30 22:42:07 +0530

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
 
 import './App.css'
-import Home from './components/Home/Home'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthProvider'
-import Login from './components/Login';
-import Register from './components/Register';
 import Solution from './components/Solution/Solution'
 import Community from  './components/Community/Community'
-import Container from './components/Container/Container';
 import Dashboard from './components/Dashboard/Dashboard';
 import Profile from './components/Profile/Profile'
-=======
-import { AuthProvider } from "./context/AuthProvider";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./App.css";
 import Home from "./components/Home/Home";
-import AddProfile from "./components/Profile/AddProfile";
 import Container from "./components/Container/Container";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -36,7 +29,10 @@ const queryClient = new QueryClient({
 function App() {
   return (
 
-    <AuthProvider>
+
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -53,22 +49,6 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-    </AuthProvider>
-=======
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-
-            <Route path="/container" element={<Container />}>
-              <Route path="profile" element={<AddProfile />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
 
